fa55014ebe9b8ed6b6f2fa062d948c39540b18ae
Abajit Dey <abajitdeyag10118@gmail.com>
2021-12-02 08:58:21 +0530

    First commit. Configured some basic files for development

---
+++ public/index.js
@@ -0,0 +1,13 @@
+import './scss/index.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const data = require('./../questions.json')
+
+const App = () => {
+    return(
+        <h1>Hello World</h1>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("container"))
